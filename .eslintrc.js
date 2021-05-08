a42b0f852a3ff08d07f39615e465a4341aecf1e7
Hamza Elaasemy <hamzaelaasemy@gmail.com>
2021-05-08 03:31:11 +0000

    project init

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "airbnb",
+        "plugin:react/recommended", 
+        "eslint:recommended"
+        ],
+    "settings": {
+        "react": {
+            "version": "detect" // detect react version
+        }
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module",
+        "import/no-extraneous-dependencies": [
+            "error", {
+               "devDependencies": true
+            }
+        ]
+    
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "import/no-extraneous-dependencies": "off",
+        "import/prefer-default-export": "off",
+        "react/jsx-filename-extension": "off",
+        "react/jsx-props-no-spreading": "off"
+    }, 
+
+};
